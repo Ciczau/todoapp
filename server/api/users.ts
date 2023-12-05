e8929d5de5b04c5504879fc1a89727656816eccb
Wiktor <123020783+Ciczau@users.noreply.github.com>
2023-12-05 15:06:25 +0100

    update

--- server/api/users.ts
+++ server/api/users.ts
@@ -1,25 +1,25 @@
-import { defineStore } from "pinia";
+import { ref } from "vue";
 import Cookies from "js-cookie";
 import { jwtDecode } from "jwt-decode";
 import { User } from "~/types/User";
+import { defineStore } from "pinia";
 
-export const useUser = defineStore("user", {
-  state: () => ({
-    user: { email: "", nick: "" } as User,
-  }),
-  actions: {
-    async getUser(): Promise<void> {
-      const res = await fetch("http://localhost:5000/api/token", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ refreshToken: Cookies.get("refreshToken") }),
-      });
-      const data = await res.json();
-      if (data.success) {
-        this.user = jwtDecode(data.token);
-      }
-    },
-  },
+export const useUser = defineStore("user", () => {
+  const user = ref({ email: "", nick: "" } as User);
+
+  async function getUser(): Promise<void> {
+    const res = await fetch("http://localhost:5000/api/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ refreshToken: Cookies.get("refreshToken") }),
+    });
+    const data = await res.json();
+    if (data.success) {
+      user.value = jwtDecode(data.token);
+    }
+  }
+
+  return { user, getUser };
 });
